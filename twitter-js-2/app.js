a046261ba84a9cad4f67fa989c5494c49faf86f2
Joseph Kholodenko <josephkholodenko@gmail.com>
2017-07-16 16:46:56 -0400

    working nunjucks integration

--- twitter-js-2/app.js
+++ twitter-js-2/app.js
@@ -1,31 +1,36 @@
 var express = require('express');
 var app = express();
 var morgan = require('morgan');
+var nunjucks = require('nunjucks');
 
-var logger = morgan('dev');
-app.use(logger);
+var locals = {
+    title: 'Some Tile',
+    people: [
+        { name: 'Gandalf' },
+        { name: 'Hermione' }
+    ]
+};
+// nunjucks.render(__dirname + '/views/index.html', locals, function(err, output){
+//     if (err) return console.error(err);
+//     console.log(output);
+// });
+
+nunjucks.configure('views', { noCache: true });
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
 
 // takes a middleware function, if no path, assumes '/'.
 //every time middleware function is called, must call next() to let it know the middleware function is done
-app.use(function(req, res, next){
-    res.on('finish', function(){
-        console.log('responded:', res.statusCode, req.method, req.path);     // subscribes to an event name!
-    }); 
-    next();
-});
+
+app.use(morgan('dev'));
 
 app.get('/', function(req, res){
-    res.send('you go tthe root route');
+    res.render('index', locals);
 });
 
-app.get('/news', function(req, res){
+app.get('/news', function(req, res, next){
     res.json({name: 'newsRoute', data: 12345});
-    next();
 });
-
-app.use(function(req, res, next){
-    console.log('response:', res.statusCode);
-})
 
 app.listen(1337, function(){
     console.log('listening on port 1337');
